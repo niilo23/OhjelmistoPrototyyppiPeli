cefc7db52fd639add2287e188aede92ca7fbc060
niilo23 <56909449+niilo23@users.noreply.github.com>
2021-05-18 21:03:15 +0300

    Tein omat skriptini
    Level loaderissa voi tehdä fade tms efekteja, siksi niin monimutkaiselta näyttävä.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+
+    public void LoadNextLevel(int sceneNum)
+    {
+        StartCoroutine(LoadLevel(sceneNum));
+    }
+
+	// Tässä voi tehdä mahdollisesti fade efektin jos tahtoo.
+    IEnumerator LoadLevel(int LevelIndex)
+    {
+        transition.SetTrigger("StartFade");
+
+        yield return new WaitForSecondsRealtime(transitionTime);
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(LevelIndex);
+    }
+
+    public void LevelReset()
+    {
+        StartCoroutine(ResetLevel());
+    }
+
+    IEnumerator ResetLevel()
+    {
+        transition.SetTrigger("StartFade");
+
+        yield return new WaitForSecondsRealtime(transitionTime);
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
