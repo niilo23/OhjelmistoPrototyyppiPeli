4248220331d659c9fa601f8dc1d501b3aa1fbd42
Niilo <56909449+niilo23@users.noreply.github.com>
2021-05-26 12:56:27 +0300

    Made pause script

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public KeyCode pauseKey = KeyCode.Escape;
+    public Canvas pauseCanvas;
+    public Button backButton;
+
+    public static bool gameIsPaused;
+
+    void Start()
+    {
+        pauseCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+	// Used in the pause menu if you want to make return button.
+    public void PressOfButton()
+    {
+        gameIsPaused = false;
+        PauseGame();
+    }
+
+	// Pauses the game (duh)
+    public void PauseGame()
+    {
+		// Freezes time, shows pause canvas (pause menu), stops audio and unlocks cursor
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            pauseCanvas.enabled = true;
+            AudioListener.pause = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            GameObject.Find("Glock").GetComponent<GunScript>().enabled = false;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseCanvas.enabled = false;
+            AudioListener.pause = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            GameObject.Find("Glock").GetComponent<GunScript>().enabled = true;
+        }
+    }
+}
